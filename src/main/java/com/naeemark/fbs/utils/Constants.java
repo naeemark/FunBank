02c3719feb35738cab75362574591b495d275639
Naeem <naeemark@gmail.com>
2022-12-11 01:13:35 +0100

    Adds Health Endpoint; #1, #6

---
+++ src/main/java/com/naeemark/fbs/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.naeemark.fbs.utils;
+
+/**
+ * Created by Naeem <naeemark@gmail.com>.
+ * <p>
+ * Created on: 2022-12-11
+ */
+public class Constants {
+    public static final String SERVICE_NAME = "FunBankService";
+}
