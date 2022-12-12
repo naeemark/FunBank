030020e151ebf6733cb901c0585718e5f5a76244
Naeem <naeemark@gmail.com>
2022-12-13 00:10:26 +0100

    Adds tests for Account Service (#14)

--- src/main/java/com/naeemark/fbs/services/AccountServiceImpl.java
+++ src/main/java/com/naeemark/fbs/services/AccountServiceImpl.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,7 +16,6 @@ import org.springframework.web.server.ResponseStatusException;
 import static com.naeemark.fbs.utils.Constants.DEFAULT_BALANCE;
 import static com.naeemark.fbs.utils.Constants.ERROR_ACCOUNT_NOT_FOUND;
 import static com.naeemark.fbs.utils.Constants.ERROR_ACCOUNT_SERVICE;
-import static com.naeemark.fbs.utils.Constants.ERROR_DUPLICATE_KEY_ATTRIBUTE;
 
 
 /**
@@ -38,9 +36,6 @@ public class AccountServiceImpl implements AccountService {
         try {
             Account account = new Account(DEFAULT_BALANCE);
             return accountRepository.save(account);
-        } catch (DataIntegrityViolationException e) {
-            logger.error(e.getMessage());
-            throw new ResponseStatusException(HttpStatus.CONFLICT, ERROR_DUPLICATE_KEY_ATTRIBUTE);
         } catch (Exception e) {
             logger.error(e.getMessage());
             throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, ERROR_ACCOUNT_SERVICE);
